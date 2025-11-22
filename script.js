1458999b57fa98dbf02524b4d9a61645b5070a6f
iTLFesttester <koki.gatos12@gmail.com>
2025-11-23 03:12:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-/* ▼ タブ切り替え処理 ▼ */
-document.querySelectorAll(".tab-button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    // タブの active を変更
-    document.querySelectorAll(".tab-button").forEach(b => b.classList.remove("active"));
-    btn.classList.add("active");
 
-    // 画面切り替え
-    const target = btn.dataset.target;
-    document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
-    document.getElementById(target).classList.add("active");
-  });
-});
